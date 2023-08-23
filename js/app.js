ba782a940124944a2db15e85a7f0562e14888945
liamswork <137915577+liamswork@users.noreply.github.com>
2023-08-23 04:57:45 +0100

    Email Validation/Array
    -Added email validation
    -Created references to add email/add image buttons, with event handlers preventing default behaviour.
    
    -addEmail() added to append inputted email to select menu as an option and into the email array.. Uses validation before entering it into the array/select form.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,20 @@
 //Reference to image element. We will manipulate the SRC and ALT attributes based on the json list of objects.
 const imageElement = document.getElementById('placeholder-image');
+//Reference to add email button. Add listener and pass event to prevent default behaviour later.
+const emailButton = document.getElementById('add-email');
+emailButton.addEventListener('click', function(event) {
+    addEmail(event);
+});
+
+const addImageButton = document.getElementById('add-image');
+addImageButton.addEventListener('click', function(event) {
+    addImage(event);
+});
+
+//Reference to the email input field, we will take the value and add to the email select when validated.
+const emailField = document.getElementById('email');
+//The email select dropdown which will be populated after validation of emails.
+const emailSelect = document.getElementById('email-list');
 
 //Client ID key, and endpoint link with key interpolated into it.
 const clientID = "T6KWxGFTJusthxNjkZ_VapSy7cBDFYUuNExIrQlnJFQ";
@@ -9,6 +24,8 @@ let endpoint = `https://api.unsplash.com/photos/random/?client_id=${clientID}&co
 let imageIndex = 0;
 //Image list which holds onto the 30 results fetched from upsplash.
 let imageList = [];
+//Email array
+let emailArray = [];
 
 
 //Fetch initial list of images. Call this function whenever we exhaust the list of 30 images. Disable this line when not working on JS functionality
@@ -30,25 +47,49 @@ function fetchImages(){
 }
 
 //Display image at current index. Index is incremented by button click.
-//TO-DO: When index reaches imageList.length, fetch new list of images.
 function newImage(){
+    //If there are images in the list, and our index is less than the length of the list
+    //then assign the current image at the list index to the image element.
     if(imageList.length > 0 && imageIndex < imageList.length){
         const currentImage = imageList[imageIndex];
         imageElement.src = currentImage.urls.regular;
         imageElement.alt = currentImage.description;
-        console.log("Displaying image @" + imageIndex)
+        //console.log("Displaying image @" + imageIndex)
     }
     if(imageIndex == imageList.length){
-        console.log("End of the image list. Fetching new list and restarting index.");
+        //console.log("End of the image list. Fetching new list and restarting index.");
         imageIndex = 0;
         fetchImages();
     }else{
-        console.log("Not at the end of the image list yet.");
+        //console.log("Not at the end of the image list yet.");
         imageIndex++;
     }
 }
 
-function addEmail(){
-    console.log("Attempting to add email");
+//This function is called when the "Add Email" button is pressed.
+//Default behaviour for the form button is prevented, then the email is validated.
+//If validation is successful, the email is added to an array of emails accessible by a dropdown menu.
+//If unsuccessful, an error message is added.
+function addEmail(event){
+    //Prevent page refresh
+    event.preventDefault();
+    //Update emailInput value with current field value.
+    let emailInput = emailField.value;
+
+    if(isValidEmail(emailInput)){
+        let newOption = document.createElement('option');
+        newOption.textContent = emailInput;
+        emailSelect.appendChild(newOption);
+        emailArray.push(emailInput);
+        console.log(emailArray);
+    }else{
+        console.log("INVALID EMAIL")
+    }
 }
+
+function addImage(event){
+
+}
+
+
 
---
+++ js/validation.js
@@ -0,0 +1,9 @@
+    //Declare regex patterns. Has a built in .test() function where you pass it a piece of text.
+
+    let namePattern = /^[a-zA-Z]+(([\'\,\.\- ][a-zA-Z ])?[a-zA-Z]*)*$/
+    let phonePattern = /^(((\+44\s?|0044\s?)?|(\(?0))((2[03489]\)?\s?\d{4}\s?\d{4})|(1[23456789]1\)?\s?\d{3}\s?\d{4})|(1[23456789][234578][0234679]\)?\s?\d{6})|(1[2579][0245][0467]\)?\s?\d{5})|(11[345678]\)?\s?\d{3}\s?\d{4})|(1[35679][234689]\s?[46789][234567]\)?\s?\d{4,5})|([389]\d{2}\s?\d{3}\s?\d{4})|([57][0-9]\s?\d{4}\s?\d{4})|(500\s?\d{6})|(7[456789]\d{2}\s?\d{6})))$/
+
+    function isValidEmail(email){
+        let emailPattern = /^((?:(?:(?:[a-zA-Z0-9][\.\-\+_]?)*)[a-zA-Z0-9])+)\@((?:(?:(?:[a-zA-Z0-9][\.\-_]?){0,62})[a-zA-Z0-9])+)\.([a-zA-Z0-9]{2,6})$/;
+        return emailPattern.test(email);
+    }
